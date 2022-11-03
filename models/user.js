472a4bcdb8f0316dfe24c4050f3898a7d0111d4e
Diego Mercado <diego.mercado1997@hotmail.com>
2022-11-02 22:58:00 -0300

    .

--- models/user.js
+++ models/user.js
@@ -1,27 +1,32 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt-nodejs')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-   email:{type:String,required:true,lowercase:true,uniqued:true},
-   password:{type:String,required:true},
-   registerDate:{type:Date,default:Date.now()}
-})
+  email: { type: String, required: true, lowercase: true, uniqued: true },
+  password: { type: String, required: true },
+  registerDate: { type: Date, default: Date.now() },
+});
 
-UserSchema.pre('save',(next)=>{
-   let user = this
+UserSchema.pre("save", (next) => {
+  let user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (error, salt) => {
+    if (error) {
+      return next(error);
+    }
+    bcrypt.hash(user.password, salt, null, (error, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
+});
 
-   bcrypt.genSalt(10,(error,salt)=>{
-      bcrypt.hash(user.password,salt,null,(error,hash)=>{
-         user.password = hash
-         next()
-      })
-   })
-})
-
-UserSchema.method.comparePassword = (password) =>{
-   let user = this
-  return bcrypt.compareSync(password,user.password)
-}
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.method.comparePassword = (password) => {
+  let user = this;
+  return bcrypt.compareSync(password, user.password);
+};
+module.exports = mongoose.model("User", UserSchema);
 // 1:09
