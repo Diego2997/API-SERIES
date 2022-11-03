fd064408d144b2bf407d092fe3e40bec0e04a19d
Diego Mercado <diego.mercado1997@hotmail.com>
2022-11-02 22:57:54 -0300

    cambios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,64 @@
-const User = require('../models/user')
-const {authServices} = require('../services')
+const User = require("../models/user");
+const { authServices } = require("../services");
 
 // ----------------CONSULTAR-------------------
-const signIn = (req,res) =>{
-    const { email,password } = req.body
+const signIn = (req, res) => {
+  const { email, password } = req.body;
 
-    if(!email){
-        return res.status(400).send({messsage:"el campo email es requerido"})
+  if (!email) {
+    return res.status(400).send("El campo email es requerido");
+  }
+  User.findOne({ email }, (error, user) => {
+    if (error) {
+      return res.status(500).send("Hubo un error", error);
     }
-
-    User.findOne({email} , (error,user) => {
-        if(error){
-            return res.status(500).send('se produjo un error' + error)
-        }
-        if(!user){
-            res.status(404).send({message:"no se encontro un usuario con el email ingresado"})
-        }
-        if(!(password && user.comparePassword(password))){
-            res.status(401).send({message:"El usuario o la clave son incorrectos"})
-        }
-
-        res.status(200).send({message:"Te has logueado correctamente",token: authServices.createToken(user)})
-    })
-}
+    if (!user) {
+      //TODO
+      return res
+        .status(400)
+        .send({ message: "No se encontro un usuario con el email ingresado" });
+    }
+    res
+      .status(200)
+      .send({ message: "Te has logueado correactamente", token: "" });
+  });
+  // if(!(password && user.comparePassword(password))){
+  //     res.status(401).send({message:"El usuario o la clave son incorrectos"})
+  // }
+};
 
 // ---------CREAR---------------------------------
-const signUp = (req,res) =>{
-    const { email,password } = req.body
+const signUp = (req, res) => {
+  const { email, password } = req.body;
 
-    User.findOne({email} , (error,user) => {
-        if(error){
-            return res.status(500).send('se produjo un error' + error)
-        }
-        if(user){
-            res.status(400).send({message:"El email ya se encuentra en uso"})
-        }
-        // SI NO EXISTE EL USUARIO CON ESE EMAIL SE CREA UN NUEVO USUARIO
-        const newUser = new User({email,password})
+  User.findOne({ email }, (error, user) => {
+    if (error) {
+      return res.status(500).send("Hubo un error al registrarse", error);
+    }
+    if (user) {
+      return res
+        .status(400)
+        .send({ message: "El email ya se encuentra en uso" });
+    }
+    const newUser = new User({
+      email: email,
+      password: password,
+    });
+    newUser.save((error) => {
+      return res.status(500).send("Se produjo un error", error);
+    });
+  });
+  res
+    .status(200)
+    .send({ message: "Te has logueado correactamente", token: "" });
+};
 
-        //------------- GUARDAR EL USUARIO NUEVO------------------
-        newUser.save((error)=>{
-            if(error){
-                return res.status(500).send('se produjo un error' + error)
-            }
-            res.status(200).send({message:"Te has registrado correctamente",token: authServices.createToken(newUser)})
-        })
-    })
-}
-
-
-
-const sayHi = (req,res) =>{
-    //TODO
-}
+const sayHi = (req, res) => {
+  //TODO
+};
 
 module.exports = {
-    signIn,
-    signUp,
-    sayHi
-}
+  signIn,
+  signUp,
+  sayHi,
+};
